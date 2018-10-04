1dccb8d15ec187b846c3539f5f54bc5831b24c55
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-04 10:44:45 -0300

    upgrade component de encuentas

--- components/PanelComponent.js
+++ components/PanelComponent.js
@@ -1,5 +1,7 @@
 import {StyleSheet,Text,View,Image,TouchableHighlight,Animated} from 'react-native';
+
 import React, { Component } from 'react';
+
 import {Scene,Router, Actions} from 'react-native-router-flux';
 
 export default class PanelComponent extends Component{
@@ -20,95 +22,84 @@ export default class PanelComponent extends Component{
     
     componentDidMount(){
       this.toggle();
-
-      setTimeout( () => {
-        this.toggle();
-      }, 200);
     }
 
     toggle(){
-      let initialValue    = this.state.expanded? this.state.maxHeight + this.state.minHeight : this.state.minHeight,
-          finalValue      = this.state.expanded? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
+      let initialValue    = this.state.expanded ? this.state.maxHeight + this.state.minHeight : this.state.minHeight,
+          finalValue      = this.state.expanded ? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
   
       this.setState({
-          expanded : !this.state.expanded
+        expanded : !this.state.expanded
       });
   
       this.state.animation.setValue(initialValue);
+
       Animated.spring(
         this.state.animation,
         {
-            toValue: finalValue
+          toValue: finalValue
         }
       ).start();
     }
     
     _setMaxHeight(event){
-        this.setState({
-          maxHeight   : event.nativeEvent.layout.height
-        });
+      this.setState({
+        maxHeight   : event.nativeEvent.layout.height
+      });
     }
 
     _setMinHeight(event){
-        this.setState({
-          minHeight   : event.nativeEvent.layout.height
-        });
+      this.setState({
+        minHeight   : event.nativeEvent.layout.height
+      });
     }
 
     render(){
-        let icon = this.icons['down'];
+      let icon = this.icons['down'];
 
-        if(this.state.expanded){
-            icon = this.icons['up'];
-        }
+      if(this.state.expanded){
+        icon = this.icons['up'];
+      }
 
-        return ( 
-            <Animated.View style={[styles.container,{height: this.state.animation}]}>
-              <View style={styles.titleContainer} onLayout={this._setMinHeight.bind(this)}>
-                <Text style={styles.title}>{this.state.title}</Text>
-                <TouchableHighlight 
-                    style={styles.button} 
-                    onPress={this.toggle.bind(this)}
-                    underlayColor="#f1f1f1">
-                    <Image
-                        style={[styles.buttonImage, { paddingRight: 10 }]}
-                        source={icon}
-                    ></Image>
-                </TouchableHighlight>
-              </View>
-                
-              <View style={styles.body} onLayout={this._setMaxHeight.bind(this)}>
-                  {this.props.children}
-              </View>
-             </Animated.View>
-        );
+      return(
+        <Animated.View style={[styles.container,{height: this.state.animation}]}>
+          <View style={styles.titleContainer} onLayout={this._setMinHeight.bind(this)}>
+            <Text style={styles.title}>{this.state.title}</Text>
+
+            <TouchableHighlight style={styles.button} onPress={this.toggle.bind(this)} underlayColor="#f1f1f1">
+              <Image style={[styles.buttonImage, { paddingRight: 10 }]} source={icon} />
+            </TouchableHighlight>
+          </View>
+
+          <View style={styles.body} onLayout={this._setMaxHeight.bind(this)}>
+            {this.props.children}
+          </View>
+        </Animated.View>
+      );
     }
 }
 
 var styles = StyleSheet.create({
     container:{
-        backgroundColor: '#fff',
-        margin:10,
-        overflow:'hidden'
+      backgroundColor: '#fff',
+      margin: 10,
+      overflow: 'hidden'
     },
     titleContainer:{
-        flexDirection: 'row'
+      flexDirection: 'row'
     },
     title:{
-        flex    : 1,
-        padding : 10,
-        color   :'#2a2f43',
-        fontWeight:'bold'
-    },
-    button:{
-
+      flex: 1,
+      padding: 10,
+      color: '#2a2f43',
+      fontWeight: 'bold'
     },
     buttonImage:{
-        width   : 30,
-        height  : 25
+      width: 30,
+      height: 25
     },
     body: {
-        padding     : 10,
-        paddingTop  : 0
+      padding: 10,
+      paddingTop: 0
     }
 });
