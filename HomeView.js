f91dfb1e97b0a53690ea858304f92748e257d431
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-05 12:09:08 -0300

    upgrade homeview

--- HomeView.js
+++ HomeView.js
@@ -1,86 +1,112 @@
 import React, { Component } from 'react';
+
 import {Text, View, StyleSheet, Image, ImageBackground, TouchableOpacity, Linking } from 'react-native';
+
 import {Scene,Router, Actions} from 'react-native-router-flux';
+
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-const iconSize = 70;
+// const icon = ();
+
+const iconSize = 50;
+
+const iconColor = "#1B76BC";
 
 export default class HomeView extends Component {
-
-
   render() {
-  
     return (
-      <View style={{flex:1, flexDirection: 'row', backgroundColor: '#eae9ef'}}>
-      <ImageBackground source={{ uri: 'https://static.vix.com/es/sites/default/files/styles/large/public/imj/nuestrorumbo/l/las-5-montanas-mas-altas-del-mundo-4.jpg?itok=gmnd5X7f' }} style={{marginTop: 30, flex: 1, opacity: 1}}>
-      
-        <View style={[styles.container, {} ]}>
-          <View style={[styles.row, { flex:18, backgroundColor: 'transparent'}]}>
-            
-            <View style={{ flex: 16, justifyContent: 'flex-end', padding: 12 }}>
-              <TouchableOpacity onPress={ () => Actions.PollsContainer() } style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 15}}>
-                  <FontAwesome5 name="bar-chart" size={iconSize} color="white" />
-                  <View style={{paddingTop: 6}}><Text style={{color: '#bdc3c7', fontSize: 20, fontWeight: '500', textAlign: 'center'}}>  Encuesta </Text></View>
-              </TouchableOpacity>
-            </View>
-            
-            <View style={{ flex: 1, backgroundColor: 'trasnparent', justifyContent: 'flex-end', alignItems: 'center'}}>
-              <View style={{ height: '50%', width: 2, marginBottom: 27, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: '#bdc3c7' }}>
+      <View style={{flex:1, flexDirection: 'row', backgroundColor: 'white', padding: 2, paddingBottom: 0}}>
+        <ImageBackground source={{ uri: 'https://k42.kn3.net/taringa/3/E/4/0/8/E/gonzaa9614/1B5.jpg' }} style={{flex: 1, opacity: 1}}>
+          <View style={[styles.container, {} ]}>
+            <View style={[styles.row, { flex: 1, backgroundColor: 'transparent'}]}>
+              <View style={{ flex: 2, justifyContent: 'flex-end', padding: 12 }}>
+                <TouchableOpacity onPress={ () => Actions.PollsContainer() } style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <View style={styles.viewIcon}>
+                    <FontAwesome5 name={'sticky-note'} size={iconSize} color={iconColor} solid />
+                  </View>
+
+                  <View style={{paddingTop: 6}}>
+                    <Text style={styles.text}>
+                      Encuesta
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+              
+              <View style={{justifyContent: 'flex-end', alignItems: 'center'}}>
+                <View style={[styles.lineVertical, {marginBottom: 12}]}>
+                </View>
+              </View>
+              
+              <View style={{flex: 2, justifyContent: 'flex-end', padding: 12 }}>
+                <TouchableOpacity onPress={ () => Linking.openURL('http://www.columbiaviajes.com.ar/contacto.php' )}style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <View style={styles.viewIcon}>
+                    <FontAwesome5 name={'smile-wink'} size={iconSize} color={iconColor} solid />
+                  </View>
+
+                  <View style={{paddingTop: 6}}>
+                    <Text style={styles.text}>
+                      Contacto
+                    </Text>
+                  </View>
+                </TouchableOpacity>
               </View>
             </View>
             
-            <View style={{ flex: 16, justifyContent: 'flex-end', padding: 12 }}>
-              <TouchableOpacity onPress={ () => Linking.openURL('http://www.columbiaviajes.com.ar/contacto.php' )}style={{justifyContent: 'center', alignItems: 'center', paddingRight: 15}}>
-                <FontAwesome5 name="user-circle" size={iconSize} color="white" />
-                <View style={{paddingTop: 6}}><Text style={{color: '#bdc3c7', fontSize: 20, fontWeight: '500', textAlign: 'center'}}> Contacto </Text></View>
-              </TouchableOpacity>
+            <View style={[styles.row, { justifyContent: 'center'}]}>
+              <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
+                <View style={[styles.lineHorizontal, styles.leftRight]}>
+
+                </View>
+
+                <View style={[styles.circulo, styles.leftRight, styles.topBottom]}>
+                </View>
+
+                <View style={[styles.lineHorizontal, styles.leftRight]}>
+                </View>
+              </View>
             </View>
             
+            <View style={[styles.row, { flex:1, backgroundColor: 'transparent'}]}>
+              <View style={{ flex: 2, padding: 12 }}>
+                <TouchableOpacity onPress={() => Actions.VoucherContainer()} style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <View style={styles.viewIcon}>
+                    <FontAwesome5 name={'list-alt'} size={iconSize} color={iconColor} solid />
+                  </View>
+
+                  <View style={{paddingTop: 6}}>
+                    <Text style={styles.text}>
+                      {`Vouchers\n e Itinerario`}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+              
+              <View style={{backgroundColor: 'transparent', justifyContent: 'flex-start', alignItems: 'center'}}>
+                <View style={[styles.lineVertical, {marginTop: 12}]}>
+                </View>
+              </View>
+              
+              <View style={{flex: 2, padding: 12 }}>
+                <TouchableOpacity onPress={ () => Actions.ClaimsContainer() } style={{justifyContent: 'center', alignItems: 'center'}}>
+                  <View style={styles.viewIcon}>
+                    <FontAwesome5 name={'wpforms'} size={iconSize} color={iconColor} solid/>
+                  </View>
+
+                  <View style={{paddingTop: 6}}>
+                    <Text style={styles.text}>
+                      {`Formulario\n de Reclamos`}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
           
-          <View style={[styles.row, { flex:1, backgroundColor: 'trasnparent', justifyContent: 'center'}]}>
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-              <View style={{flexDirection: 'column', width: '33%', marginRight: 27, height: 2, alignSelf: 'center', backgroundColor:'#bdc3c7'}}>
-              </View>
-              <View style={[styles.circulo, {}]}>
-              </View>
-              <View style={{flexDirection: 'column', width: '33%', marginLeft: 27, height: 2, alignSelf: 'center', backgroundColor:'#bdc3c7'}}>
-              </View>
-            </View>
+          <View style={[styles.row, {flex: 1, paddingTop: 5, paddingBottom: 5, justifyContent: 'center', alignItems: 'center', backgroundColor: '#E6F3FC' }]}>
+            <Image source={{ uri: 'http://www.columbiaviajes.com.ar/img/columbia-logo.png' }} style={{ resizeMode: 'contain', height: '90%', marginLeft: 50, width: '60%'}}/>
           </View>
-          
-          <View style={[styles.row, { flex:18, backgroundColor: 'trasnparent'}]}>
-          
-            <View style={{ flex: 16, padding: 12 }}>
-              <TouchableOpacity onPress={ () => Actions.VoucherContainer() }style={{justifyContent: 'center', alignItems: 'center', paddingLeft: 15}}>
-                <FontAwesome5 name="globe" size={iconSize} color="white" />
-                <View style={{paddingTop: 6}}><Text style={{color: '#bdc3c7', fontSize: 20, fontWeight: '500', textAlign: 'center'}}>{`Vouchers\n e Itinerario`}</Text></View>
-              </TouchableOpacity>
-            </View>
-            
-            <View style={{ flex: 1, backgroundColor: 'trasnparent', justifyContent: 'flex-start', alignItems: 'center'}}>
-              <View style={{ height: '50%', width: 2, marginTop: 27, justifyContent: 'center', alignItems: 'center', flexDirection: 'column', backgroundColor: '#bdc3c7' }}>
-              </View>
-            </View>
-            
-            <View style={{ flex: 16, padding: 12 }}>
-              <TouchableOpacity onPress={ () => Actions.ClaimsContainer() } style={{justifyContent: 'center', alignItems: 'center', paddingRight: 15}}>
-                <FontAwesome5 name="wpforms" size={iconSize} color="white" />
-                <View style={{paddingTop: 6}}><Text style={{color: '#bdc3c7', fontSize: 20, fontWeight: '500', textAlign: 'center'}}>{`Formulario\n de Reclamos`}</Text></View>
-              </TouchableOpacity>
-            </View>
-          </View>
-        </View>
-        <View style={[styles.row, { flex:3, backgroundColor: 'trasnparent', justifyContent:'center'}]}>
-        </View>
-        
-        <View style={[styles.row, { paddingBottom: 25, flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'trasnparent' }]}>
-        <Image
-          source={{ uri: 'http://www.columbiaviajes.com.ar/img/columbia-logo.png' }}
-          style={{ resizeMode: 'contain', height: '90%',padding: 30, marginLeft: 30, width: '60%'}}
-        />
-        </View>
-      </ImageBackground>
+        </ImageBackground>
       </View>
     );
   }
@@ -96,11 +122,47 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: 'row'
   },
+  lineHorizontal:{
+    flex: 1,
+    width: '33%', 
+    height: 2, 
+    alignSelf: 'center', 
+    backgroundColor:'#bdc3c7'
+  },
+  leftRight:{
+    marginLeft: 12,
+    marginRight: 12
+  },
+  topBottom:{
+    marginTop: 12,
+    marginBottom: 12
+  },
+  viewIcon:{
+    display: 'flex', 
+    backgroundColor: '#EAEAEA', 
+    borderRadius: 5,
+    padding: 15,
+
+  },
+  lineVertical:{
+    height: '50%', 
+    width: 1, 
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    flexDirection: 'column', 
+    backgroundColor: '#bdc3c7'
+  },
   circulo: {
      width: 7,
      height: 7,
      borderRadius: 10,
      backgroundColor: 'white'
+  },
+  text:{
+    color: '#FCFCFC', 
+    fontSize: 19, 
+    fontWeight: '400', 
+    textAlign: 'center'
   }
 });
 
