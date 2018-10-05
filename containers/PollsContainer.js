ce5aebc0f0dac34688326a78eda4e622686108e7
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-05 17:47:46 -0300

    upgrade poolsContainer

--- containers/PollsContainer.js
+++ containers/PollsContainer.js
@@ -1,89 +1,73 @@
 import React, { Component } from 'react';
+
 import {Text, View, StyleSheet, Image, ScrollView, ImageBackground, TouchableOpacity, TouchableHighlight, Linking } from 'react-native';
+
 import {Scene,Router, Actions} from 'react-native-router-flux';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+
 import CollapsibleList from 'react-native-collapsible-list'
+
 import Div from './ModelContainer/index.js';
+
 import Panel from '../components/PanelComponent.js';
+
 import Footer from '../components/Footer.js';
+
 import * as t from 'tcomb-form-native'
+
 var Form = t.form.Form;
+
 import * as tvalidation from 'tcomb-validation';
+
 var validate = tvalidation.validate;
 
-
 var Person = t.struct({
-  '¿Lorem ipsum dolor sit amet?': t.String,              // a required string
-  '¿Lorem ipsum dolor sit amet2?': t.String,  // an optional string
-  '¿Lorem ipsum dolor sit amet3?': t.String ,               // a required number
+  '¿Lorem ipsum dolor sit amet?': t.String,
+  '¿Lorem ipsum dolor sit amet2?': t.String,
+  '¿Lorem ipsum dolor sit amet3?': t.String ,
   '¿Lorem ipsum dolor sit amet4?': t.String,
   '¿Lorem ipsum dolor sit amet5?': t.String,
   '¿Lorem ipsum dolor sit amet6?': t.String,
   //fechaDelReclamo: t.Date
 });
 
-let myFormatFunction = (format,date) =>{
+let myFormatFunction=(format,date)=>{
     return moment(date).format(format);
 }
 
-let options = {
-    fields: {
-    }
+let options={
+  fields:{
+  }
 };
 
-export default class PollsContainer extends Component {
+export default class PollsContainer extends Component{
   constructor(props){
     super(props);
     
-    this.state = {
+    this.state={
     };
   }
 
-  render () {
-    return (
-      <View style={{display: 'flex', flex: 1}}>
-        <ScrollView style={styles.container}>
-          <View style={{display: 'flex', flex: 1}}>
-            <Div name="Encuesta" icon="bar-chart" footer={false}>
-              <View style={styles.container}>
-                  <Panel title="Encuesta de calidad 1">
-                    <View style={{flex: 1, justifyContent: 'space-around', padding: 8}}>
-                      <Form
-                        ref="form"
-                        type={Person}
-                        options={options}
-                      />
+  render(){
+    return(
+      <Div name="Encuesta" icon="bar-chart">
+        <View style={styles.container}>
+            <Panel title="Encuesta de calidad 1">
+              <Form ref="form" type={Person} options={options}/>
 
-                      <TouchableHighlight style={styles.button} onPress={ () => this.onPress() } underlayColor='#99d9f4'>
-                        <Text style={[styles.buttonText, {}]}>Enviar</Text>
-                      </TouchableHighlight>
-                    </View>
-                  </Panel>
+              <TouchableHighlight style={styles.button} onPress={ () => this.onPress() } underlayColor='#99d9f4'>
+                <Text style={[styles.buttonText, {}]}>Enviar</Text>
+              </TouchableHighlight>
+            </Panel>
 
-                  <Panel title="Encuesta de calidad 2">
-                    <View style={{flex: 1, justifyContent: 'space-around', padding: 8}}>
-                      <Form
-                        ref="form"
-                        type={Person}
-                        options={options}
-                      />
+            <Panel title="Encuesta de calidad 2">
+              <Form ref="form" type={Person} options={options}/>
 
-                      <TouchableHighlight style={styles.button} onPress={ () => this.onPress() } underlayColor='#99d9f4'>
-                        <Text style={[styles.buttonText, {}]}>Enviar</Text>
-                      </TouchableHighlight>
-                    </View>
-                  </Panel>
-              </View>
-            </Div>
-          </View>
-
-
-        </ScrollView>
-
-        <View style={{display: 'flex'}}>
-          <Footer/>
+              <TouchableHighlight style={styles.button} onPress={ () => this.onPress() } underlayColor='#99d9f4'>
+                <Text style={[styles.buttonText, {}]}>Enviar</Text>
+              </TouchableHighlight>
+            </Panel>
         </View>
-      </View>
+      </Div>
     )
   }
 }
