5118c2c7f5257fdfc2fceffc409d9c9b192f00e9
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-08 17:56:39 -0300

    upload VoucherContainer

--- containers/VoucherContainer.js
+++ containers/VoucherContainer.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
-import {Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 import {Actions} from 'react-native-router-flux';
 
 import Div from './ModelContainer/index.js';
 
-export default class VoucherContainer extends Component {
+import FileComponent from '../components/FileComponent.js';
+
+export default class VoucherContainer extends Component{
   constructor(props){
     super(props);
     
@@ -14,74 +16,20 @@ export default class VoucherContainer extends Component {
     };
   }
 
-  render () {
-    return (
+  render(){
+    return(
       <Div name="Voucher e Initerarios" icon='bar-chart'>
-        <View style={{ flexDirection: 'column', flex:1, justifyContent: 'flex-start', alignItems: 'center'}}>
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 28, width: '90%', marginBottom: 7, marginTop: 7, backgroundColor: '#1B9CFC'}}>
-            <View style={{paddingLeft: 5}}>
-              <Text style={[styles.texts, {} ]}> 
-                Itinerario 1 
-              </Text>
-            </View>
-            
-            <TouchableOpacity onPress={ () => Actions.pop() }>
-              <View style={{paddingRight: 10}}>
-                <Text>
-                  {/*<FontAwesomeIcon icon="cloud-download" size={25} color="white" />*/}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 28, width: '90%', marginBottom: 7, marginTop: 7, backgroundColor: '#1B9CFC'}}>
-            <View style={{paddingLeft: 5}}>
-              <Text style={[styles.texts, {} ]}> 
-                Itinerario 2 
-              </Text>
-            </View>
-            
-            <TouchableOpacity onPress={ () => Actions.pop() }>
-              <View style={{paddingRight: 10}}>
-                <Text>
-                  {/*<FontAwesomeIcon icon="cloud-download" size={25} color="white" />*/}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 28, width: '90%', marginBottom: 7, marginTop: 7, backgroundColor: '#1B9CFC'}}>
-            <View style={{paddingLeft: 5}}>
-              <Text style={[styles.texts, {} ]}> 
-                Voucher 1 
-              </Text>
-            </View>
-            
-            <TouchableOpacity onPress={ () => Actions.pop() }>
-              <View style={{paddingRight: 10}}>
-                <Text>
-                  {/*<FontAwesomeIcon icon="cloud-download" size={25} color="white" />*/}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-          
-          <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', height: 28, width: '90%', marginBottom: 7, marginTop: 7, backgroundColor: '#1B9CFC'}}>
-            <View style={{paddingLeft: 5}}>
-              <Text style={[styles.texts, {} ]}> 
-                Voucher 1 
-              </Text>
-            </View>
-            
-            <TouchableOpacity onPress={ () => Actions.pop() }>
-              <View style={{paddingRight: 10}}>
-                <Text>
-                  {/*<FontAwesomeIcon icon="cloud-download" size={25} color="white" />*/}
-                </Text>
-              </View>
-            </TouchableOpacity>
-          </View>
-        </View>
+      {
+        function(){
+          let contentFiles = [];
+
+          for(i = 0; i < 5; i++){
+            contentFiles.push(<FileComponent name="Test" style={styles.fileComponent}/>);
+          }
+
+          return contentFiles;
+        }()
+      }
       </Div>
     )
   }
@@ -91,4 +39,8 @@ const styles = StyleSheet.create({
   texts: {
     color: 'white'
   },
+  fileComponent: {
+    marginTop: 6,
+    marginBottom: 6
+  }
 });
