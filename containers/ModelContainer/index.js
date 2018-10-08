9d358c16b8a23e24f1d9b826bdc210755839a287
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-08 17:57:12 -0300

    upload layout

--- containers/ModelContainer/index.js
+++ containers/ModelContainer/index.js
@@ -8,35 +8,54 @@ import BackLeft from '../../components/BackLeft.js';
 
 import Footer from '../../components/Footer.js';
 
+import Preloader from '../../components/Preloader.js';
+
 export default class PollsContainer extends Component{
   constructor(props){
     super(props);
 
     this.state={
-      heightParent: ""
+      heightParent: "",
+      loading: true
     };
+  }
+
+  componentDidMount(){
+    setTimeout( () => {
+      this.setState({
+        loading: false
+      });
+    }, 350);
   }
 
   render(){
     return(
-      <View style={[{ flex: 1, flexDirection: 'column' }, {}]}>
+      <View style={[{flex: 1, flexDirection: 'column'}, {}]}>
         {
         (this.props.backleft != undefined && this.props.backleft == false)  ? null
         : (<BackLeft name={this.props.name} icon={this.props.icon} />)
         }
         <View style={{display: 'flex', flex: 1}} onLayout={(event) => this.setState({heightParent: event.nativeEvent.layout.height})}>
           <ScrollView style={{display: 'flex'}}>
-            <View style={{display: 'flex', minHeight: this.state.heightParent!="" ? this.state.heightParent : null}}>
-              <View style={styles.container}>
+            <View style={[styles.childrenScrollView, {minHeight: this.state.heightParent!="" ? this.state.heightParent : null}]}>
+              <View style={this.state.loading ? [styles.loading, styles.container] : styles.container}>
                 <View>
                   <Text>
                   {this.props.title}
                   </Text>
                 </View>
 
-                <View style={{ display: 'flex', flex: 1 }}>
-                  {this.props.children}
-                </View>
+                {
+                this.state.loading
+                  ?
+                    <Preloader/>
+                  :
+                    <View style={(this.props.container != undefined && this.props.container == false) ? [{}, styles.parentContainer] : [styles.propParentContainer, styles.parentContainer]}>
+                      {
+                      this.props.children
+                      }
+                    </View>
+                }
               </View>
 
               {
@@ -52,7 +71,24 @@ export default class PollsContainer extends Component{
 }
 
 const styles = StyleSheet.create({
+  childrenScrollView:{
+    display: 'flex',
+  },
   container:{
-    padding: 10,
+    padding: 10
+  },
+  loading: {
+    flex: 1
+  },
+  parentContainer: {
+    display: 'flex',
+    flex: 1,
+  },
+  propParentContainer:{
+    display: 'flex',
+    flex: 1,
+    backgroundColor: 'white',
+    borderRadius: 2,
+    padding: 10
   }
 });
