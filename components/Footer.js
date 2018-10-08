0f894d093d934d91c6a5f8c6cb7e553214926837
Vidal Tomás <vtcheqeeh@gmail.com>
2018-10-08 17:55:02 -0300

    upload footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+
 import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+
 import {Scene,Router, Actions} from 'react-native-router-flux';
 
-export default class AssetExample extends Component {
-  render() {
-    return (
-          <View style={{height: 80, marginTop: 'auto', backgroundColor: '#48BBEC'}}>
-            <Image source={require('../img/footer.jpg')} style={ styles.footerImage }>
-            </Image>
-          </View>
+export default class Footer extends Component{
+  render(){
+    return(
+      <View style={styles.body}>
+        <Image source={require('../img/footer.jpg')} style={styles.footerImage} />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  body:{
+    height: 85, 
+    marginTop: 'auto', 
+    backgroundColor: '#48BBEC',
+    borderRadius: 1
+  },
   footerImage:{
     flex: 1,
     alignSelf: 'center',
