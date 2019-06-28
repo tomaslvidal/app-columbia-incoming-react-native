52cdbce18ffdde411cc4c512031c72aac13dfb65
cheqeeh <vidaltomas08@gmail.com>
2019-06-28 12:06:09 -0300

    refactory and integration of api with destinations

--- containers/DestinationContainer/List/DestinationListContainer.jsx
+++ containers/DestinationContainer/List/DestinationListContainer.jsx
@@ -1,78 +1,74 @@
 import React, { Component } from 'react';
 
-import { connect } from 'react-redux';
-
 import { View, ListView, TouchableOpacity, StyleSheet, Linking } from 'react-native';
 
 import DestinationBox from '../../../components/DestinationBoxComponent'
-
-import BackLeft from '../../../components/BackLeftComponent'
 
 import Div from '../../../layouts/default';
 
 import axios from 'axios';
 
 class DestinationList extends Component {
-  constructor(props){
-    super(props);
+    constructor(props){
+        super(props);
 
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
-    this.state = {
-      dataSource: ds,
-      items: {},
-      loading: true
-    };
-  }
+        this.state = {
+            dataSource: ds,
+            items: [],
+            loading: true
+        };
+    }
 
-  updateDataSource(data){
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows(data)
-    })
-  }
-
-  componentDidMount(){
-    axios.get('http://columbiaapp.eviajes.online/api/destinations')
-    .then(response => {
+    updateDataSource(data){
         this.setState({
-          items: response.data,
-          loading: false
-        }, () => {
-          this.updateDataSource(this.state.items);
+            dataSource: this.state.dataSource.cloneWithRows(data)
+        })
+    }
+
+    componentDidMount(){
+        axios.get('http://www.columbiaviajes.com/admin/services/api_destinosMapas.php')
+        .then(res => {
+            this.setState({
+                items: res.data,
+                loading: false
+            }, () => {
+                this.updateDataSource(this.state.items);
+            });
         });
-    });
-  }
-
-  handlePress(item){
-    if(item.url==undefined){
-      this.props.navigation.navigate('DestinationDetail', { item: item });
     }
-    else{
-      Linking.openURL(item.url)
-    }
-  }
 
-  render() {
-    return (
-      <Div name="Destinos" icon="wpforms" loading={this.state.loading}>
-        <ListView
-          enableEmptySections={true}
-          dataSource={this.state.dataSource}
-          renderRow={ (item) => {
-          return (
-            <TouchableOpacity onPress={ () => this.handlePress(item) }>
-              <DestinationBox destination={item}/>
-            </TouchableOpacity>
-          )
-          }}
-        />
-      </Div>
-    );
-  }
+    handlePress(item){
+        if(typeof item.url=== 'undefined'){
+            this.props.navigation.navigate('DestinationDetail', { item: item });
+        }
+        else{
+            Linking.openURL(item.url)
+        }
+    }
+
+    render() {
+        return (
+            <Div name="Destinos" icon="wpforms" loading={this.state.loading}>
+                <ListView
+                    enableEmptySections={true}
+                    dataSource={this.state.dataSource}
+                    renderRow={ item => {
+                        return(
+                            <TouchableOpacity onPress={ () => this.handlePress(item) }>
+                                <DestinationBox item={item}/>
+                            </TouchableOpacity>
+                        );
+                    }}
+                />
+            </Div>
+        );
+    }
 }
-  
+
 const styles = StyleSheet.create({
 
 });
 
-export default connect()(DestinationList);
+export default DestinationList;
