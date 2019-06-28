b22d3ca86b183d80f21618d3f6644fba186e989f
cheqeeh <vidaltomas08@gmail.com>
2019-06-28 12:07:28 -0300

    refactory and integration with api 'tours'

--- containers/ToursContainer/ItemContainer.jsx
+++ containers/ToursContainer/ItemContainer.jsx
@@ -16,197 +16,195 @@ import Div from 'ColumbiaIncoming/layouts/default';
 
 const COORDINATES = {};
 
-export default class ItemContainer extends Component {
-  constructor(props){
-    super(props);
+export default class ItemContainer extends Component{
+    constructor(props){
+        super(props);
 
-    this.state = { 
-        item: this.props.item,
-        maxWidth: 0
-    };
+        this.state = {};
 
-    this.scrollView = this.scrollView.bind(this);
+        this.scrollView = this.scrollView.bind(this);
 
-    this.onLinkPress = this.onLinkPress.bind(this);
-  }
+        this.onLinkPress = this.onLinkPress.bind(this);
+    }
 
-  scrollView(e){
-    this.div.scroll_view.scrollTo({ x: 0, y: COORDINATES[e]-80});
-  }
+    scrollView(e){
+        this.div.scroll_view.scrollTo({ x: 0, y: COORDINATES[e]-80 });
+    }
 
     onLinkPress(href){
         Linking.openURL(href);
     }
 
-  render() {
-    return (
-        <View>
-          <View style={{ marginLeft: -20, marginRight: -20 }}>
-            <Image indicatorProps={{
-                size: 80,
-                key: this.state.item.image1,
-                borderWidth: 0,
-                color: 'rgba(150, 150, 150, 1)',
-                unfilledColor: 'rgba(200, 200, 200, 0.2)'
-            }} indicator={Progress} source={{uri: this.state.item.image1}} style={[styles.footerImage]} />
-          </View>
+    componentWillMount(){
+        console.log("items: ", this.props.item.image);
+    }
 
-          <View style={styles.box}>
-            <Text style={styles.textTitle}>{this.state.item.title}</Text>
+    render() {
+        return (
+            <View>
+                <View style={{ marginLeft: -20, marginRight: -20 }}>
+                    <Image indicatorProps={{
+                        size: 80,
+                        key: this.props.item.image,
+                        borderWidth: 0,
+                        color: 'rgba(150, 150, 150, 1)',
+                        unfilledColor: 'rgba(200, 200, 200, 0.2)'
+                    }} indicator={Progress} source={{uri: this.props.item.image}} style={[styles.footerImage]} />
+                </View>
 
-              <HTML html={this.state.item.description!=undefined ? this.state.item.description : ''} alterChildren = { node => {
-                        if(node.name === 'p'){
-                            if(typeof node.attribs['style'] != "undefined"){
-                                let arrayProperties = node.attribs['style'].split(';').map(item => item.trim());
+                <View style={styles.box}>
+                    <Text style={styles.textTitle}>{this.props.item.title}</Text>
 
-                                if(arrayProperties.length>0){
-                                    ['start', 'end'].forEach(value => {
-                                        let find_index = arrayProperties.findIndex(item => typeof item != 'undefined' ? (item.indexOf(value) != -1 && item.indexOf('text-align') != -1) : false);
+                    <HTML 
+                        html={typeof this.props.item.description.es !== "undefined" ? this.props.item.description.es : ''} 
+                        alterChildren = { node => {
+                            if(node.name === 'p'){
+                                if(typeof node.attribs['style'] !== "undefined"){
+                                    let arrayProperties = node.attribs['style'].split(';').map(item => item.trim());
 
-                                        if(find_index != -1){
-                                            delete arrayProperties[find_index];
-                                        }
-                                    });
+                                    if(arrayProperties.length>0){
+                                        ['start', 'end'].forEach(value => {
+                                            let find_index = arrayProperties.findIndex(item => typeof item !== 'undefined' ? (item.indexOf(value) != -1 && item.indexOf('text-align') != -1) : false);
 
-                                    node.attribs['style'] = arrayProperties.join(';');
-                                }
-                            }
-                        }
-
-                        return node.children;
-                    }
-                }
-                renderers = {{
-                    img: (parameters) => {
-                        let key = Math.random().toString(36).substr(2, 8);
-                        console.log("x: ", parameters);
-                        return(
-                            <Image 
-                                source={{ uri: parameters.src }}
-                                key={key+'-'+parameters.src}
-                                style={{
-                                    width: ((parameters) => {
-                                        if(typeof parameters.width != "undefined"){
-                                            return !isNaN(Number(parameters.width)) ? Number(parameters.width) : '100%';
-                                        }
-
-                                        if(typeof parameters.style != "undefined"){
-                                            let array = parameters.style.split(';');
-
-                                            array = array.map(item => item.trim());
-
-                                            array = array.filter(item => item.indexOf('width')!=-1);
-
-                                            if(array.length>0){
-                                                array = array[0].split('width:');
-
-                                                array = array.map(item => item.trim());
-
-                                                return !isNaN(Number(array[1].slice(0, -2))) ? Number(array[1].slice(0, -2)) : '100%';
+                                            if(find_index != -1){
+                                                delete arrayProperties[find_index];
                                             }
-                                        }
+                                        });
 
-                                        return '100%';
-                                    })(parameters),
-                                    height: ((parameters) => {
-                                        if(typeof parameters.height != "undefined"){
-                                            return !isNaN(Number(parameters.height)) ? Number(parameters.height) : 180;
-                                        }
-
-                                        if(typeof parameters.style != "undefined"){
-                                            let array = parameters.style.split(';');
-
-                                            array = array.map(item => item.trim());
-
-                                            array = array.filter(item => item.indexOf('height')!=-1);
-
-                                            if(array.length>0){
-                                                array = array[0].split('height:');
-
-                                                array = array.map(item => item.trim());
-
-                                                return !isNaN(Number(array[1].slice(0, -2))) ? Number(array[1].slice(0, -2)) : 180;
-                                            }
-                                        }
-
-                                        return 180
-                                    })(parameters),
-                                }
-                            }/>
-                        );
-                    },
-                    a: (parameters, two, three, four) => {
-                        let key = Math.random().toString(36).substr(2, 5);
-
-                        if(typeof parameters.href != "undefined"){
-                            if(parameters.href == "#"){
-                                if(typeof parameters.id != "undefined"){
-                                    if(parameters.id.length != 0){
-                                            return(
-                                                <View key={key} ref={ (ref) => this[parameters.id] = ref } onLayout={ ({nativeEvent}) => {
-                                                    if(this[parameters.id]) {
-                                                        this[parameters.id].measure((x, y, width, height, pageX, pageY) => {
-                                                            COORDINATES[parameters.id] = pageY;
-                                                        });
-                                                    }
-                                                }}>
-                                                    <Text >
-                                                        {two[0]}
-                                                    </Text>
-                                                </View>
-                                            );
+                                        node.attribs['style'] = arrayProperties.join(';');
                                     }
                                 }
                             }
-                            else if(parameters.href.indexOf('#')==0){
-                                return(
-                                    <TouchableOpacity key={key} onPress={() => { this.scrollView(parameters.href.slice(1)) } }>
-                                        <Text>{two[0]}</Text>
-                                    </TouchableOpacity>
-                                );
-                            }
-                            else{
-                                return(
-                                    <TouchableOpacity key={key} onPress={() => { this.onLinkPress(parameters.href) } }>
-                                        {two}
-                                    </TouchableOpacity>
-                                );
-                            }
-                        }
 
-                        return(two);
-                    }
-                }}
-                />
-          </View>
-        </View>
-    );
-  }
+                            return node.children;
+                        }}
+                        renderers = {{
+                            img: (parameters) => {
+                                let key = Math.random().toString(36).substr(2, 8);
+                                
+                                return(
+                                    <Image 
+                                        source={{ uri: parameters.src }}
+                                        key={key+'-'+parameters.src}
+                                        style={{
+                                            width: ((parameters) => {
+                                                if(typeof parameters.width !== "undefined"){
+                                                    return !isNaN(Number(parameters.width)) ? Number(parameters.width) : '100%';
+                                                }
+
+                                                if(typeof parameters.style !== "undefined"){
+                                                    let array = parameters.style.split(';');
+
+                                                    array = array.map(item => item.trim());
+
+                                                    array = array.filter(item => item.indexOf('width')!=-1);
+
+                                                    if(array.length>0){
+                                                        array = array[0].split('width:');
+
+                                                        array = array.map(item => item.trim());
+
+                                                        return !isNaN(Number(array[1].slice(0, -2))) ? Number(array[1].slice(0, -2)) : '100%';
+                                                    }
+                                                }
+
+                                                return '100%';
+                                            })(parameters),
+                                            height: ((parameters) => {
+                                                if(typeof parameters.height !== "undefined"){
+                                                    return !isNaN(Number(parameters.height)) ? Number(parameters.height) : 180;
+                                                }
+
+                                                if(typeof parameters.style !== "undefined"){
+                                                    let array = parameters.style.split(';');
+
+                                                    array = array.map(item => item.trim());
+
+                                                    array = array.filter(item => item.indexOf('height')!=-1);
+
+                                                    if(array.length>0){
+                                                        array = array[0].split('height:');
+
+                                                        array = array.map(item => item.trim());
+
+                                                        return !isNaN(Number(array[1].slice(0, -2))) ? Number(array[1].slice(0, -2)) : 180;
+                                                    }
+                                                }
+
+                                                return 180
+                                            })(parameters),
+                                        }
+                                    }/>
+                                );
+                            },
+                            a: (parameters, two, three, four) => {
+                                let key = Math.random().toString(36).substr(2, 5);
+
+                                if(typeof parameters.href !== "undefined"){
+                                    if(parameters.href == "#"){
+                                        if(typeof parameters.id !== "undefined"){
+                                            if(parameters.id.length != 0){
+                                                    return(
+                                                        <View key={key} ref={ (ref) => this[parameters.id] = ref } onLayout={ ({nativeEvent}) => {
+                                                            if(this[parameters.id]) {
+                                                                this[parameters.id].measure((x, y, width, height, pageX, pageY) => {
+                                                                    COORDINATES[parameters.id] = pageY;
+                                                                });
+                                                            }
+                                                        }}>
+                                                            <Text >
+                                                                {two[0]}
+                                                            </Text>
+                                                        </View>
+                                                    );
+                                            }
+                                        }
+                                    }
+                                    else if(parameters.href.indexOf('#') === 0){
+                                        return(
+                                            <TouchableOpacity key={key} onPress={() => { this.scrollView(parameters.href.slice(1)) } }>
+                                                <Text>{two[0]}</Text>
+                                            </TouchableOpacity>
+                                        );
+                                    }
+                                    else{
+                                        return(
+                                            <TouchableOpacity key={key} onPress={() => { this.onLinkPress(parameters.href) } }>
+                                                {two}
+                                            </TouchableOpacity>
+                                        );
+                                    }
+                                }
+
+                                return(two);
+                            }
+                        }}
+                        />
+                </View>
+            </View>
+        );
+    }
 }
 
-const tagsStyles = {
-  
-};
-
 const styles = StyleSheet.create({
-  textTitle: {
-    fontSize: 30,
-    fontWeight: '500',
-    marginBottom: 8
-  },
-  footerImage: {
-    paddingTop: 10,
-    marginTop: 'auto',
-    marginBottom:'auto',
-    height: 130,
-    width: '100%',
-    marginTop: -27
-  },
-  box: {
-    backgroundColor: '#f5fcff',
-    marginLeft: -15,
-    marginRight: -15,
-    padding: 8,
-    marginBottom: 10
-  },
+    textTitle: {
+        fontSize: 30,
+        fontWeight: '500',
+        marginBottom: 8
+    },
+    footerImage: {
+        paddingTop: 10,
+        marginTop: 'auto',
+        marginBottom:'auto',
+        height: 130,
+        width: '100%',
+        marginTop: -27
+    },
+    box: {
+        backgroundColor: '#f5fcff',
+        marginLeft: -15,
+        marginRight: -15,
+        padding: 8,
+        marginBottom: 10
+    }
 });
--- containers/ToursContainer/ToursContainer.jsx
+++ containers/ToursContainer/ToursContainer.jsx
@@ -8,16 +8,17 @@ import Panel from 'ColumbiaIncoming/components/PanelComponent';
 
 import ItemContainer from './ItemContainer';
 
+import axios from 'axios';
+
 export default class ToursContainer extends Component {
     constructor(props) {
         super(props)
         const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
         this.state = {
-          dataSource: ds,
+            dataSource: ds,
             items: [],
-            loading: true,
-            run: true
+            loading: true
         }
     }
 
@@ -27,61 +28,40 @@ export default class ToursContainer extends Component {
         });
     }
 
-    componentDidMount(){
-        setTimeout(() => {
+    componentWillMount(){
+        axios({
+            url: 'http://www.columbiaviajes.com/admin/services/api_tour.php',
+            method: 'POST',
+            data: {
+                id: null
+            }
+        })
+        .then(res => {
+            console.log("x: ", res);
             this.setState({
-                items: [
-                    {
-                        image1: 'https://e00-ar-marca.uecdn.es/claro/assets/multimedia/imagenes/2019/06/11/15602607596976.jpg',
-                        title: 'Test1',
-                        description: `
-                            <div>
-                                <img src="https://rfg.eviajes.online/vue/assets/img/preloaderRFG.281f3450.png" />
-                                
-                                <p> Probando el componente......</p>
-                            </div>
-                        `
-                    },
-                    {
-                        image1: 'http://hertz.eviajes.online/banner_hertz.jpg',
-                        title: 'Test2',
-                        description: `
-                        <div>
-                                <img src="https://rfg.eviajes.online/vue/assets/img/preloaderRFG.281f3450.png" />
-                                
-                                <p> Probando el componente......</p>
-                            </div>
-                        `
-                    },
-                    {
-                        image1: 'http://hertz.eviajes.online/banner_hertz.jpg',
-                        title: 'Test3',
-                        description: `
-                            <div>
-                                <img src="https://rfg.eviajes.online/vue/assets/img/preloaderRFG.281f3450.png" />
-                                
-                                <p> Probando el componente......</p>
-                            </div>
-                        `
-                    }
-                ],
+                items: res.data,
                 loading: false
-            }, () => {
-                this.updateDataSource(this.state.items);
             });
-        }, 3000);
+        })
+        .catch(e => {
+            throw e;
+
+            console.log(e);
+        })
     }
-    
-    render() {
+
+    render(){
         return (
-            <Div name="Tours" container={false} loading={!this.state.run || this.state.loading}>
-                {this.state.items.map((item, index) => {
+            <Div name="Tours" container={false} loading={this.state.loading}>
+                {
+                this.state.items.map((item, index) => {
                     return(
                         <Panel key={index} title={item.title}>
                             <ItemContainer key={index} item={item} />
                         </Panel>
                     );
-                })}
+                })
+                }
             </Div>
         );
     }
