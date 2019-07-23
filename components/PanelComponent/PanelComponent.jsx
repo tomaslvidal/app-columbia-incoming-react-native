b1ec4d110d6d1e77eed0ba5ea96c8dc58765f382
cheqeeh <vidaltomas08@gmail.com>
2019-07-23 16:00:18 -0300

    update front and improvements, panelcomponent

--- components/PanelComponent/PanelComponent.jsx
+++ components/PanelComponent/PanelComponent.jsx
@@ -1,116 +1,113 @@
 import { StyleSheet, Text, View, Image, TouchableHighlight, Animated } from 'react-native';
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import { faCaretDown, faCaretUp } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCaretDown, faCaretUp);
 
 export default class PanelComponent extends Component{
-  constructor(props){
-    super(props);
+    constructor(props){
+        super(props);
 
-    this.icons = {
-      'up'    : 'caret-up',
-      'down'  : 'caret-down'
-    };
-
-    this.state = {
-      title : props.title,
-      expanded : false,
-      animation : new Animated.Value(),
-    };
-  }
-
-  componentDidMount(){
-    this.state.animation.setValue(40);
-  }
-
-  toggle(){
-    let initialValue = this.state.expanded ? this.state.maxHeight + this.state.minHeight : this.state.minHeight;
-
-    let finalValue = this.state.expanded ? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
-
-    this.setState({
-      expanded : !this.state.expanded
-    });
-
-    this.state.animation.setValue(initialValue);
-
-    Animated.spring(
-      this.state.animation,
-      {
-        toValue: finalValue
-      }
-    ).start();
-  }
-  
-  _setMaxHeight(event){
-    this.setState({
-      maxHeight: event.nativeEvent.layout.height
-    });
-  }
-
-  _setMinHeight(event){
-    this.setState({
-      minHeight: event.nativeEvent.layout.height
-    });
-  }
-
-  render(){
-    let icon = this.icons.down;
-
-    if(this.state.expanded){
-      icon = this.icons.up;
+        this.state = {
+            title : props.title,
+            expanded : false,
+            animation : new Animated.Value(46.85714340209961)
+        };
     }
 
-    return(
-      <Animated.View style={[styles.container, {height: this.state.animation}]}>
-        <View ref="viewMinHeight" style={styles.titleContainer} onLayout={(e) => this._setMinHeight(e)}>
-          <Text style={styles.title}>{this.state.title}</Text>
+    toggle(){
+        let initialValue = this.state.expanded ? this.state.maxHeight + this.state.minHeight : this.state.minHeight;
 
-          <TouchableHighlight style={styles.button} onPress={(e) => this.toggle(e)} underlayColor="#f1f1f1">
-            <FontAwesome5 name={icon} size={27} color={'#009EE0'} solid />
-          </TouchableHighlight>
-        </View>
+        let finalValue = this.state.expanded ? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
 
-        <View style={styles.body} >
-          <View style={styles.parent} onLayout={e => this._setMaxHeight(e)}>
-            {this.props.children}
-          </View>
-        </View>
-      </Animated.View>
-    );
-  }
+        this.setState({
+            expanded : !this.state.expanded
+        }, () => {
+            this.state.animation.setValue(initialValue);
+
+            Animated.spring(
+                this.state.animation,
+                {
+                    toValue: finalValue + (this.state.expanded ? 11 : 0)
+                }
+            ).start();
+        });
+
+    }
+
+    _setMaxHeight(event){
+        this.setState({
+            maxHeight: event.nativeEvent.layout.height
+        });
+    }
+
+    _setMinHeight(event){
+        this.setState({
+            minHeight: event.nativeEvent.layout.height
+        });
+    }
+
+    render(){
+        return(
+            <Fragment>
+                <Animated.View style={[styles.container, { height: this.state.animation }]}>
+                    <TouchableHighlight onPress={(e) => this.toggle(e)} underlayColor="#f1f1f1">
+                        <View ref="viewMinHeight" style={styles.titleContainer} onLayout={(e) => this._setMinHeight(e)}>
+                            <Text style={styles.title}>{this.state.title}</Text>
+
+                                <FontAwesomeIcon size={27} color="#009EE0" icon={['fas', this.state.expanded ? 'caret-up' : 'caret-down']} />
+                        </View>
+                    </TouchableHighlight>
+
+                    <View style={styles.body} >
+                        <View style={styles.parent} onLayout={e => this._setMaxHeight(e)}>
+                            {this.props.children}
+                        </View>
+                    </View>
+                </Animated.View>
+            </Fragment>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    backgroundColor: '#fff',
-    marginTop: 5,
-    marginBottom: 5,
-    overflow: 'hidden'
-  },
-  titleContainer:{
-    flexDirection: 'row',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 10,
-    marginBottom: 8,
-    minHeight: 40,
-  },
-  title:{
-    flex: 1,
-    color: '#2a2f43',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  body: {
-    padding: 8,
-    paddingTop: 0,
-  },
-  parent: {
-    backgroundColor: '#FFFFFF',
-    padding: 15,
-    borderRadius: 3
-  }
+    container:{
+        backgroundColor: '#f8f8f8',
+        marginTop: 5,
+        marginBottom: 22,
+        overflow: 'hidden',
+        borderColor: '#e9e9e9e1',
+        borderWidth: 1
+    },
+    titleContainer:{
+        flexDirection: 'row',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 10,
+        minHeight: 40
+    },
+    title:{
+        flex: 1,
+        color: '#2a2f43',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    body: {
+        padding: 8,
+        paddingTop: 0,
+    },
+    parent: {
+        backgroundColor: '#fff',
+        padding: 15,
+        borderRadius: 3,
+        borderColor: '#e9e9e9e1',
+        borderWidth: 1
+    }
 });
