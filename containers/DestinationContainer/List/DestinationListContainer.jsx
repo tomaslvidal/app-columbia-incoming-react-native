6d997b657cdd89e0eccd90a7211a907ddd0c4c66
cheqeeh <vidaltomas08@gmail.com>
2019-10-24 16:27:22 -0300

    update

--- containers/DestinationContainer/List/DestinationListContainer.jsx
+++ containers/DestinationContainer/List/DestinationListContainer.jsx
@@ -37,17 +37,37 @@ class DestinationList extends Component {
         this.onRefresh = this.onRefresh.bind(this);
     }
 
-    fetchDestinations(){
-        axios.get('http://www.columbiaviajes.com/admin/services/api_destinosMapas.php')
-        .then(res => {
-            this.props.setDestinations({
-                items: res.data,
-                pending: false,
-                loading: false,
-                is_refreshing: false,
-                empty: res.data.length === 0 ? true : false
+    fetchDestinations(error = ''){
+        if(!error.length || error === 'x00003'){
+            axios.get('http://www.columbiaviajes.com/admin/services/api_destinosMapas.php')
+            .then(res => {
+                if(res.data instanceof Array){
+                    if(res.data.length){
+                        if(typeof res.data[0].image !== 'undefined'){
+                            this.props.setDestinations({
+                                items: res.data,
+                                pending: false,
+                                loading: false,
+                                is_refreshing: false,
+                                empty: res.data.length === 0 ? true : false
+                            });
+
+                            return;
+                        }
+                    }
+                }
+
+                if(!error.length){
+                    throw new Error('x00003');
+                }
+                else{
+                    throw new Error('x00003l');
+                }
+            })
+            .catch((e) => {
+                this.fetchDestinations(e.message);
             });
-        });
+        }
     }
 
     componentDidMount(){
@@ -104,45 +124,50 @@ class DestinationList extends Component {
             </TouchableOpacity>
         );
 
-        return (
-            <Div name="Destinos" icon="wpforms" loading={this.props.destinations.loading} state_scroll_view={false}>
-                {
-                    this.props.destinations.items.length > 0 ?
+        let Content = () => {
+            if(typeof this.props.destinations !== 'undefined'){
+                if(this.props.destinations.items.length > 0){
+                    return(
                         <FlatList
                             data={this.props.destinations.items}
                             keyExtractor={(item, index) => index.toString()}
                             renderItem={renderItem}
-                            refreshControl={
+                            refreshControl= {
                                 <RefreshControl
                                     refreshing={this.props.destinations.is_refreshing}
                                     onRefresh={this.onRefresh}
                                 />
                             }
-                            // ListFooterComponent={this.renderFooter}
-                            // onEndReached={this.handleLoadMore}
                         />
-                    :
-                        (
-                            <View style={{ alignItems: 'center' }}>
-                                <FontAwesomeIcon 
-                                    size={37} 
-                                    color={"#10db7a"} 
-                                    icon={['fas', 'plane-departure']}
-                                />
-
-                                <FontAwesomeIcon 
-                                    size={25}
-                                    style={{ top: -30 }}
-                                    color={"#575958"} 
-                                    icon={['fas', 'times']}
-                                />
-
-                                <Text style={{ fontSize: 16, textAlign: 'center', marginTop: -17 }}>
-                                    No destinations found
-                                </Text>
-                            </View>
-                        )
+                    );
                 }
+            }
+
+            return(
+                <View style={{ alignItems: 'center' }}>
+                    <FontAwesomeIcon 
+                        size={37} 
+                        color={"#10db7a"} 
+                        icon={['fas', 'plane-departure']}
+                    />
+
+                    <FontAwesomeIcon 
+                        size={25}
+                        style={{ top: -30 }}
+                        color={"#575958"} 
+                        icon={['fas', 'times']}
+                    />
+
+                    <Text style={{ fontSize: 16, textAlign: 'center', marginTop: -17 }}>
+                        No destinations found
+                    </Text>
+                </View>
+            );
+        }
+
+        return (
+            <Div name="Destinos" icon="wpforms" loading={this.props.destinations.loading} state_scroll_view={false}>
+                <Content />
             </Div>
         );
     }
